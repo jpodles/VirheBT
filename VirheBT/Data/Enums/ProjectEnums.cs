e8d4d030d15a66bb44a2611b86d4cd5da1ea2b17
jpodles <jakubpodles98@gmail.com>
2021-05-25 13:52:15 +0200

    Add project files.

---
+++ VirheBT/Data/Enums/ProjectEnums.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirheBT.Data.Enums
+{
+
+    public enum ProjectStatus
+    {
+        OnTrack,
+        Canceled,
+        Finished
+    }
+}
+
