fd08f3a5f30d6a6201db62042ca86bb672a23082
jpodles <jakubpodles98@gmail.com>
2021-05-31 15:19:25 +0200

    add PDF print to dashboard

---
+++ VirheBT/wwwroot/JS/scripts.js
@@ -0,0 +1,17 @@
+﻿function GeneratePDF() {
+    var element = document.getElementById('dashboard');
+    var opt = {
+
+        filename: 'report.pdf',
+        margin: 3,
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: { scale: 1 },
+        jsPDF: { unit: 'mm', format: 'a3', orientation: 'landscape' }
+    };
+
+    // New Promise-based usage:
+    html2pdf().set(opt).from(element).save();
+
+
+
+}
